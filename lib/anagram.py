bea448bec5102eba7cc59e9038d651cc76b2f232
Scott Schwab <scottschwab86@gmail.com>
2023-07-27 21:15:45 -0500

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_words):
+        matching_words = []
+        for possible_word in possible_words:
+            if sorted(list(self.word)) == sorted(list(possible_word)):
+                matching_words.append(possible_word)
+        return matching_words
